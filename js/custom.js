f45dd9303ca4f987dfb3d086e699be7c9f8b67b9
yogesh <webvillage55@gmail.com>
2022-08-07 14:37:26 +0530

    home page issues

---
+++ js/custom.js
@@ -0,0 +1,19 @@
+
+
+
+
+
+  // A $( document ).ready() block.
+$( document ).ready(function() {
+
+
+    // video controler
+    $('.video').parent().click(function () {
+        if($(this).children(".video").get(0).paused){       
+          $(this).children(".video").get(0).play();   
+          $(this).children(".playpause").fadeOut();
+        }else{$(this).children(".video").get(0).pause();
+            $(this).children(".playpause").fadeIn();
+          }
+      });
+});
